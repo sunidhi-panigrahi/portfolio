355def185602929b7d81ff15b89c9436b0c97dc9
Suraj Kumar Sikhar <166533093+sunidhi-panigrahi@users.noreply.github.com>
2025-04-20 13:18:26 +0530

    w

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite'; // eslint-disable-line
 import react from '@vitejs/plugin-react'; // eslint-disable-line
 
 export default defineConfig({
-  base: '/',
+  base: '/portfolio/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-          return null;
-        },
-      },
-    },
-  },
 });
